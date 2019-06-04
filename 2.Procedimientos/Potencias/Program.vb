09324e754e797154801a6d60308303ce3c813295
caicer <kill.enzo.caicer@gmail.com>
2019-06-03 21:52:17 -0300

    Update Program.vb

--- 2.Procedimientos/Potencias/Program.vb
+++ 2.Procedimientos/Potencias/Program.vb
@@ -1,25 +1,21 @@
 Imports System
-
+private Exponente as byte = 2
 Module Program
-    Dim Numero, numerodos, numerotres, numerocinco As Int16
+    Dim Numero As Int16
+    
     Sub Main()
         Console.WriteLine("ingrese un numero")
         Numero = Console.ReadLine()
-        Console.WriteLine("elevado a la dos es: " & ExponenteDos(Numero))
-        Console.WriteLine("elevado a la tres es: " & ExponenteTres(Numero))
-        Console.WriteLine("elevado a la cinco es: " & ExponenteCinco(Numero))
+        Console.WriteLine("elevado a la dos es: " & Exponente(Numero))
+        Console.WriteLine("elevado a la tres es: " & Exponente(Numero))
+        Exponente(Numero)
+        Console.WriteLine("elevado a la cinco es: " & Exponente(Numero))
     End Sub
-    Function ExponenteDos(Numero) As Int64
-        Numerodos = Numero ^ 2
+    Function Exponente(Numero) As Int64
+        
+        Numero = Numero ^ exponente
+        exponente+=1
         Return Numerodos
     End Function
-    Function ExponenteTres(Numero) As Int64
-        Numerotres = Numero ^ 3
-        Return Numerotres
-    End Function
-    Function ExponenteCinco(Numero) As Int64
-        Numerocinco = Numero ^ 5
-        Return Numerocinco
-    End Function
-
+    
 End Module
